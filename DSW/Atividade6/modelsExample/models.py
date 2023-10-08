7a95f42e7997440f269b0fddeeb76bc6a2d1045c
Daniel <danieldfb@gmail.com>
2023-10-08 00:37:30 -0300

    estudos

--- DSW/Atividade6/modelsExample/models.py
+++ DSW/Atividade6/modelsExample/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class SeloFonografico(models.Model):
+    
+    selo_fonografico = models.CharField(
+        max_length=50,
+        verbose_name='Noma da Gravadora',
+        blank=True,
+        null=True,
+    )
+    
+    def __str__(self):
+        return self.selo_fonografico
 class Sebo(models.Model):
     
     nome = models.CharField(
@@ -16,12 +27,13 @@ class Sebo(models.Model):
         null=True,
     )
     
-    selo_fonografico = models.CharField(
-        max_length=50,
-        verbose_name='Noma da Gravadora',
+    selo = models.ForeignKey(
+        SeloFonografico,
+        on_delete=models.CASCADE,
         blank=True,
         null=True,
-    )
+        )
+    
     
     ano = models.IntegerField(
         blank=True,
@@ -51,3 +63,44 @@ class Sebo(models.Model):
     
     def __str__(self):
         return self.nome
+    
+class Artista(models.Model):
+    
+    nome = models.CharField(
+        verbose_name="nome do artista",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    
+    disco = models.ManyToManyField(
+        Sebo,
+        blank=True,
+        null=True,
+    )
+    
+    def __str__(self):
+        return self.nome
+    
+    
+# class Choice(models.Model):
+#     choice_text = models.CharField(
+#         max_length= 200,
+#         verbose_name= " Qual o numero de votos",
+#         blank= True,
+#     )
+#     votes = models.IntegerField(
+#         verbose_name="Número de votos",
+#         blank=True,
+#     )
+#     question = models.ForeignKey(
+#         Sebo,
+#         verbose_name="Questão",
+#         on_delete=models.CASCADE,
+#         )
+    
+#     def __str__ (self):
+#         return self.choice_text
+    
+#     O selo fonográfico deve deixar de ser apenas um atributo de texto do disco e passar a ser uma classe. Um disco deve ter apenas um selo fonográfico e um selo fonográfico poderá estar em vários discos;
+# Crie um modelo Artista. O modelo artista poderá possuir vários discos e um disco poderá ser gravado por vários artistas.
