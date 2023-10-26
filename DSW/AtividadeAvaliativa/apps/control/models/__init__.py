ad0500fc34a5918615043e6c93b13bd742351678
Daniel <danieldfb@gmail.com>
2023-10-26 16:18:13 -0300

    atividade

---
+++ DSW/AtividadeAvaliativa/apps/control/models/__init__.py
@@ -0,0 +1,9 @@
+from .cidade import Cidade
+from .aluno import Aluno
+from .curso import Curso
+
+__all__ = [
+    Cidade,
+    Aluno,
+    Curso,
+]
---
+++ DSW/AtividadeAvaliativa/apps/control/views/__init__.py
@@ -0,0 +1,12 @@
+from .cadastrar import cadastrar_aluno
+from .listar import listar_alunos
+from .pesquisar import pesquisar_alunos
+from .visualizar import visualizar_aluno
+
+
+__all__ = [
+    cadastrar_aluno,
+    listar_alunos,
+    pesquisar_alunos,
+    visualizar_aluno,
+]
