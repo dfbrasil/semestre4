e87fa3d33db58574d33c571a1619bddc7dfb6eec
Daniel <danieldfb@gmail.com>
2023-10-27 16:13:26 -0300

    last updates

--- DSW/AtividadeAvaliativa/apps/control/models/cidade.py
+++ DSW/AtividadeAvaliativa/apps/control/models/cidade.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Cidade(models.Model):
-    nome = models.CharField(max_length=255)
-    sigla_estado = models.CharField(max_length=2)
+    nome = models.CharField(
+        verbose_name='Cidade:',
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    sigla_estado = models.CharField(
+        verbose_name='UF:',
+        max_length=2,
+        blank=True,
+        null=True,)
 
     def __str__(self):
         return self.nome
--- DSW/AtividadeAvaliativa/apps/control/models/curso.py
+++ DSW/AtividadeAvaliativa/apps/control/models/curso.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Curso(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(
+        verbose_name='Curso:',
+        max_length=255,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.nome
