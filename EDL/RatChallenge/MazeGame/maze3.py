6c0261d4ab3b7699368dcc4e597eda0d37b96597
Daniel <danieldfb@gmail.com>
2023-10-31 17:15:14 -0300

    att

--- EDL/RatChallenge/MazeGame/maze3.py
+++ EDL/RatChallenge/MazeGame/maze3.py
@@ -1,119 +1,112 @@
-
-# Import pygame
 import pygame
- 
-# Initialise pygame
+
 pygame.init()
 
-n = 10
-m = 10
+def load_maze_from_file(filename):
+    maze = []
+    posRatX, posRatY = 0, 0
+    posCheeseX, posCheeseY = 0, 0
 
-maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 0, 1, 0, 0, 0, 0, 1, 1],
-    [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
-    [1, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-    [1, 1, 1, 0, 0, 1, 1, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+    with open(filename, 'r') as file:
+        for i, line in enumerate(file):
+            row = []
+            for j, char in enumerate(line.strip()):
+                if char == '0':
+                    row.append(0)
+                elif char == '1':
+                    row.append(1)
+                elif char == 'm':
+                    row.append(2)
+                    posRatX, posRatY = j, i
+                elif char == 'c':
+                    row.append(3)
+                    posCheeseX, posCheeseY = j, i
+            maze.append(row)
+    n = len(maze)
+    m = len(maze[0])
+    return maze, n, m, posRatX, posRatY, posCheeseX, posCheeseY
+
+maze, n, m, posRatX, posRatY, posCheeseX, posCheeseY = load_maze_from_file('maze.txt')
+
+cell_size = min(800 // m, 600 // n)
+width = m * cell_size
+height = n * cell_size
 
 pygame.display.set_caption('Maze Hunter')
 
 Icon = pygame.image.load('/home/danieldfb/Repositório - NOVO/semestre4/EDL/RatChallenge/MazeGame/images/rat.png')
 
 pygame.display.set_icon(Icon)
- 
-size = width,height = 600, 600
-screen = pygame.display.set_mode(size)
-cell_size = width // n
 
-#background screen color
+size = (width, height)
+screen = pygame.display.set_mode(size)
+
+# background screen color
 color = (0, 0, 0)
- 
+
 ratImage = pygame.image.load('/home/danieldfb/Repositório - NOVO/semestre4/EDL/RatChallenge/MazeGame/images/rat.png')
 cheeseImage = pygame.image.load('/home/danieldfb/Repositório - NOVO/semestre4/EDL/RatChallenge/MazeGame/images/cheese.png')
 pathImage = pygame.image.load('/home/danieldfb/Repositório - NOVO/semestre4/EDL/RatChallenge/MazeGame/images/path.jpg')
 wallImage = pygame.image.load('/home/danieldfb/Repositório - NOVO/semestre4/EDL/RatChallenge/MazeGame/images/wall.png')
- 
+
 RAT_IMAGE_SIZE = (40, 40)
 CHEESE_IMAGE_SIZE = (40, 40)
 PATH_IMAGE_SIZE = (50, 50)
 WALL_IMAGE_SIZE = (50, 50)
- 
-# Scale the image to your needed size
+
+# Redimensione a imagem para o tamanho necessário
 ratImage = pygame.transform.scale(ratImage, RAT_IMAGE_SIZE)
 cheeseImage = pygame.transform.scale(cheeseImage, CHEESE_IMAGE_SIZE)
-pathImage = pygame.transform.scale(pathImage,PATH_IMAGE_SIZE)
+pathImage = pygame.transform.scale(pathImage, PATH_IMAGE_SIZE)
 wallImage = pygame.transform.scale(wallImage, WALL_IMAGE_SIZE)
- 
-posRatX = 0
-posRatY = 0
-RAT_POSITION = (posRatX,posRatY)
 
-posCheeseX = 500
-posCheeseY = 500
-CHEESE_POSITION = (posCheeseX,posCheeseY)
- 
+# Inicialize as posições iniciais
+posRatX = posRatX * cell_size
+posRatY = posRatY * cell_size
+posCheeseX = posCheeseX * cell_size
+posCheeseY = posCheeseY * cell_size
+
+RAT_POSITION = (posRatX, posRatY)
+CHEESE_POSITION = (posCheeseX, posCheeseY)
+
 running = True
 
-print(size)
-
 pygame.time.wait(1000)
-pixels = 12
+pixels = cell_size  # Tamanho do movimento baseado na célula
 
 while running:
-     
-    # screen.fill((255, 255, 255)) 
-
-    screen.blit(ratImage, (posRatX, posRatY))
-    
-    screen.blit(cheeseImage, (posCheeseX, posCheeseY))
-    
     for i in range(n):
         for j in range(m):
             x = j * cell_size
             y = i * cell_size
             if maze[i][j] == 0:
-                screen.blit( pathImage, (x, y))
+                screen.blit(pathImage, (x, y))
             elif maze[i][j] == 1:
                 screen.blit(wallImage, (x, y))
-  
-    for event in pygame.event.get(): 
-  
-        if event.type == pygame.QUIT: 
-            run = False
-            pygame.quit() 
-            quit() 
+                
+    screen.blit(ratImage, (posRatX, posRatY))
+    screen.blit(cheeseImage, (posCheeseX, posCheeseY))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            quit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 if posRatX > 0:
                     posRatX -= pixels
-
             if event.key == pygame.K_RIGHT:
                 if posRatX < width - RAT_IMAGE_SIZE[0]:
                     posRatX += pixels
-
             if event.key == pygame.K_UP:
                 if posRatY > 0:
                     posRatY -= pixels
-
             if event.key == pygame.K_DOWN:
                 if posRatY < height - RAT_IMAGE_SIZE[1]:
                     posRatY += pixels
-  
-    pygame.display.update() 
-    
-    CHEESE_POSITION = (posCheeseX,posCheeseY)
- 
+
+    pygame.display.update()
+
     screen.fill(color)
-
-    screen.blit(ratImage, RAT_POSITION)
-    
-    screen.blit(cheeseImage, CHEESE_POSITION)
- 
-
